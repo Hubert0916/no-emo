42c092263863146bd547c3598e8d07d5862dc35e
Yuhan Qiu <h237k77777@gmail.com>
2025-03-18 01:21:07 +0800

    feat: add stack and tab

--- App.js
+++ App.js
@@ -1,36 +1,88 @@
-import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, Image } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Hook = props => {
-  const [isClicked, setIsClick] = useState(false);
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function ProfileStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function HomeScreen() {
+  const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>
-        {isClicked? 'Hi, ' + props.name: '點我看看'}
-      </Text>
-      <Button
-        onPress={() => {
-          setIsClick(true);
-        }}
-        disabled={isClicked}
-        title={isClicked ? '快點做專題' : '這是個按鈕'}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.navigate('Details')}>
+        點我拜託🥺
+      </Button>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>紅中自摸🤑</Text>
+      <Image
+        source={require('./assets/mahjong.png')}
+        style={{ width: 300, height: 300 }}
       />
     </View>
-  );
-};
+  )
+}
 
-const App = () => {
+function ProfileScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-    }}>
-      <Hook name="sb" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.navigate('Settings')}>
+        不要點我拜託🥺
+      </Button>
     </View>
   );
-};
+}
 
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>什麼都沒有🥸</Text>
+    </View>
+  )
+}
+
+function MyTab() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Profile" component={ProfileStack} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTab />
+    </NavigationContainer>
+  );
+}
