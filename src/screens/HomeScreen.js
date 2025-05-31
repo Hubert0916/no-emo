2568eeeef88eb22cb7cce8ff23edd83f92eb6ae4
Joanne <chieh.mg11@nycu.edu.tw>
2025-06-01 04:27:26 +0800

    revised the homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,20 +7,12 @@ export default function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: "center" }}>
+    <View style={styles.container}>
       <Image
         source={require("../assets/gradient.png")}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          width: "100%",
-          height: "100%",
-        }}
+        style={styles.backgroundImage} 
       />
-      <Text style={{ fontSize: 48, color: "white", top: "20%" }}>
+      <Text style={styles.title}>
         今天好嗎?
       </Text>
       <CloudAnimation />
